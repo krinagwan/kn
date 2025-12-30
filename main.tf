e18aa0cfafdb1cb2657aa14eee2e1812fb308b0c
krinagwan <krinagwan@gmail.com>
2025-12-30 15:35:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,11 +16,20 @@ provider "azurerm" {
   subscription_id = "ddb23ce9-0db9-442f-9bbd-5a86d4102828"
 }
 
-resource "azurerm_resource_group" "action" {
-  name     = "action"
-  location = "West Europe"
+data "azurerm_resource_group" "action" {
+  name     = "dev-cus-poc-rg-0001"
 }
-resource "azurerm_resource_group" "action2" {
-  name     = "action2"
-  location = "West Europe"
+
+resource "azurerm_storage_account" "example" {
+  name                     = "actionuntname"
+  resource_group_name      = data.azurerm_resource_group.action.name
+  location                 = "CentralUS"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
 }
+
+
