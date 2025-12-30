0d5507fe9d35195d5e8189dc80bae8266c3b2a44
krinagwan <krinagwan@gmail.com>
2025-12-30 15:50:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,12 @@ terraform {
       version = "4.54.0"
     }
   }
-
+ backend "azurerm" {
+    resource_group_name   = "dev-cus-poc-rg-0001"
+    storage_account_name  = "actionuntname"
+    container_name        = "ctactionuntname"
+    key                   = "actiondev.terraform.tfstate"
+  }
 
 }
 
@@ -31,5 +36,18 @@ resource "azurerm_storage_account" "example" {
     environment = "staging"
   }
 }
+
+resource "azurerm_storage_account" "example2" {
+  name                     = "actionuntname2"
+  resource_group_name      = data.azurerm_resource_group.action.name
+  location                 = "CentralUS"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
+
 
 
